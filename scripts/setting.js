9e4c1fd87347a7687e21828e7cd8afda8fecc4c1
taiki-fw <taiki_52@outlook.jp>
2019-12-08 03:37:48 +0900

    外部moduleであるasyncの削除

--- scripts/setting.js
+++ scripts/setting.js
@@ -1,73 +1,39 @@
-// var GoogleSpreadsheet = require("google-spreadsheet");
-// var async = require("async");
+var GoogleSpreadsheet = require("google-spreadsheet");
 
-// var doc = new GoogleSpreadsheet("1V0yILdCOmle772i8nAiuhbzW4IHof7L7vQAjGOabo_4"); // ここはスプレッドシートごとに書き換える必要がある
-// var sheet;
+var doc = new GoogleSpreadsheet("1V0yILdCOmle772i8nAiuhbzW4IHof7L7vQAjGOabo_4"); // ここはスプレッドシートごとに書き換える必要がある
+var sheet;
 
-// var time = "10:00";
-// var name = "飛んで";
-// var status = "退勤";
-// var working_time = 6;
+var time = "10:00";
+var name = "飛んで";
+var status = "退勤";
+var working_time = 6;
 
-// async.series(
-//   [
-//     function setAuth(step) {
-//       console.log(step);
-//       var creds = require("../MATTKnock-f8684ee7b88f.json");
-//       doc.useServiceAccountAuth(creds, step);
-//     },
-//     function getInfoAndWorksheets(step) {
-//       doc.getInfo(function(err, info) {
-//         sheet = info.worksheets[0];
-//         step();
-//       });
-//     },
-//     function setData(step) {
-//       sheet.addRow(
-//         {
-//           現在時刻: time,
-//           名前: name,
-//           出勤退勤: status,
-//           労働時間: working_time
-//         },
-//         function(row) {
-//           console.log(row);
-//         }
-//       );
-//       step();
-//     },
-//     function workingWithCells(step) {
-//       const COLUMNS = {
-//         current_time: 0,
-//         name: 1,
-//         status: 2,
-//         working_time: 3
-//       };
-//       sheet.getCells(
-//         {
-//           "min-row": 1,
-//           "max-row": 6,
-//           "return-empty": true
-//         },
-//         function(err, cells) {
-//           for (let i = 0; i < cells.length / sheet.colCount; i++) {
-//             const current_time =
-//               cells[i * sheet.colCount + COLUMNS.current_time].value;
-//             const name = cells[i * sheet.colCount + COLUMNS.name].value;
-//             const status = cells[i * sheet.colCount + COLUMNS.status].value;
-//             const working_time =
-//               cells[i * sheet.colCount + COLUMNS.working_time].value;
-//             console.log(
-//               current_time + " " + name + " " + status + "" + working_time
-//             );
-//           }
-//         }
-//       );
-//     }
-//   ],
-//   function(err) {
-//     if (err) {
-//       console.log("Error: " + err);
-//     }
-//   }
-// );
+function setAuth() {
+  return new Promise(function (resolve, reject) {
+    var creds = require("../MATTKnock-f8684ee7b88f.json");
+    doc.useServiceAccountAuth(creds, function () {
+      doc.getInfo(function (err, info) {
+        if (err) {
+          reject("認証error");
+        }
+        sheet = info.worksheets[0];
+        resolve("認証成功")
+      });
+    });
+  })
+}
+
+async function setData() {
+  const res = await setAuth()
+  console.log(res);
+  sheet.addRow(
+    {
+      現在時刻: time,
+      名前: name,
+      出勤退勤: status,
+      労働時間: working_time
+    }
+  );
+}
+
+setData();
