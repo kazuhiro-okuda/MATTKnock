36acce5c71d0225d42ddc46e608177e5c893c158
taiki-fw <taiki_52@outlook.jp>
2019-12-07 17:04:07 +0900

    GoogleSpreadSheetの入力の実装

--- scripts/setting.js
+++ scripts/setting.js
@@ -12,11 +12,24 @@ async.series(
       doc.useServiceAccountAuth(creds, step);
     },
     function getInfoAndWorksheets(step) {
-      doc.getInfo(function(err, info) {
-        // console.log(info);
+      doc.getInfo(function (err, info) {
         sheet = info.worksheets[0];
+        // sheet.setHeaderRow(["現在時刻", "名前", "出勤/退勤", "労働時間"]); //async
         step();
       });
+    },
+    function setData() {
+      sheet.addRow(
+        {
+          現在時刻: "10:00",
+          名前: "tester",
+          "出勤退勤": "退勤",
+          労働時間: 6
+        },
+        function (row) {
+          console.log(row);
+        }
+      );
     },
     function workingWithCells(step) {
       const COLUMNS = {
@@ -25,46 +38,47 @@ async.series(
         status: 2,
         working_time: 3
       };
-      // sheet.getCells(
-      //   {
-      //     "min-row": 1,
-      //     "max-row": 6,
-      //     "return-empty": true
-      //   },
-      //   function(err, cells) {
-      //     // console.log(cells.length);
-      //     // console.log(sheet.colCount);
-      //     for (let i = 0; i < cells.length / sheet.colCount; i++) {
-      //       const current_time =
-      //         cells[i * sheet.colCount + COLUMNS.current_time].value;
-      //       const name = cells[i * sheet.colCount + COLUMNS.name].value;
-      //       const status = cells[i * sheet.colCount + COLUMNS.status].value;
-      //       const working_time =
-      //         cells[i * sheet.colCount + COLUMNS.working_time].value;
-      //       console.log(
-      //         current_time + " " + name + " " + status + "" + working_time
-      //       );
-      //     }
-      //   }
-      // );
       sheet.getCells(
         {
           "min-row": 1,
-          "max-row": 2,
+          "max-row": 6,
           "return-empty": true
         },
-        function(err, cells) {
-          var cell = cells[0];
-          var cell2 = cells[1];
-          cell.value = "test";
-          cell2.value = "test2";
-          cell.save();
-          cell2.save();
+        function (err, cells) {
+          // console.log(cells.length);
+          // console.log(sheet.colCount);
+          for (let i = 0; i < cells.length / sheet.colCount; i++) {
+            const current_time =
+              cells[i * sheet.colCount + COLUMNS.current_time].value;
+            const name = cells[i * sheet.colCount + COLUMNS.name].value;
+            const status = cells[i * sheet.colCount + COLUMNS.status].value;
+            const working_time =
+              cells[i * sheet.colCount + COLUMNS.working_time].value;
+            console.log(
+              current_time + " " + name + " " + status + "" + working_time
+            );
+          }
         }
       );
+      // sheet.getCells(
+      //   {
+      //     "min-row": 1,
+      //     "max-row": 2,
+      //     "return-empty": true
+      //   },
+      //   function(err, cells) {
+      //     // console.log(doc.getLastRow());
+
+      //     var cell = cells[0];
+      //     var cell2 = cells[1];
+      //     cell.setValue("書き込み");
+      //     cell2.value = "通過";
+      //     cell2.save();
+      //   }
+      // );
     }
   ],
-  function(err) {
+  function (err) {
     if (err) {
       console.log("Error: " + err);
     }
